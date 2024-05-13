62d198d49ef9bad51f3627123841571b23b62dde
Kieran Cattermole <kctffs@gmail.com>
2024-05-13 13:58:40 +0000

    made board for class method

--- run.py
+++ run.py
@@ -1,24 +1,48 @@
-class Board:
+
+print("   0  1  2  3  4  5  6  7 ")
+for xaxis in range(8):
+    rows = ""
+    for yaxis in range(8):
+        water = " ~ "
+        rows = rows + water
+    print(xaxis, rows)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#class Board:
     """
     Class for the games board which will hold lists, create the boards size, number of battleships,
     and the players name. Class will also print.
     """
-    def __init__(self, size, num_ships, player_name):
+    #def __init__(self, size, num_ships, player_name):
 
-def validate_chosen_guess():
+#def validate_chosen_guess():
     """
     In place to secure guesses are playable from the player
     """
 
-def chosen_guess():
+#def chosen_guess():
     """
     Creating the input for the coord guessed by the player on the board.
     """
 
 
-def run_game():
+#def run_game():
     """
     Starts the battleships game and creates the board by correlating with the Board class; choosing
     size, number of battleships and name. Will reset previous plays.
     """
-
