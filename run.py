68ffed40446a340c40dcbcef464d48528275cb06
Kieran Cattermole <kctffs@gmail.com>
2024-05-13 23:01:33 +0000

    added shuffle to imported library, created random number and letter for boat placement / direction

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
-from random import randint
-
+from random import randint, shuffle
+"""
 def run_game(hit, miss, sunk):
-    """
+    
     Creates the board for the game and prints it and holds lists for the players actions
-    """
+    
     print("   0  1  2  3  4  5  6  7 ")
     
     position = 0
@@ -27,9 +27,12 @@ miss = []
 sunk = []
 
 run_game(hit, miss, sunk)
+"""
 
-
-
+create_ships = randint(0, 63)
+direction_list = ["N", "S", "E", "W"]
+shuffle(direction_list)
+print(create_ships, direction_list[0])
 
 
 
