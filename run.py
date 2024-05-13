a69bfe91dae20791823329f1b998960e09d6a0ad
Kieran Cattermole <kctffs@gmail.com>
2024-05-13 14:49:12 +0000

    added lists to hold hit, miss or sunk ships

--- run.py
+++ run.py
@@ -1,56 +1,34 @@
-print("   0  1  2  3  4  5  6  7 ")
-
-hit = [63]
-position = 0
-for xaxis in range(8):
-    rows = ""
-    for yaxis in range(8):
-        water = " ~ "
-        if position in hit:
-            water = " X "
-        rows = rows + water
-        position += 1
-    print(xaxis, rows)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#class Board:
-    """
-    Class for the games board which will hold lists, create the boards size, number of battleships,
-    and the players name. Class will also print.
-    """
-    #def __init__(self, size, num_ships, player_name):
-        #self.size = size
-        #self.num_ships = num_ships
-        #self.player_name = player_name
-
-#def validate_chosen_guess():
-    """
-    In place to secure guesses are playable from the player
-    """
-
-#def chosen_guess():
-    """
-    Creating the input for the coord guessed by the player on the board.
-    """
-
-
-#def run_game():
+def run_game():
     """
     Starts the battleships game and creates the board by correlating with the Board class; choosing
     size, number of battleships and name. Will reset previous plays.
     """
+    print("   0  1  2  3  4  5  6  7 ")
+    miss = []
+    hit = []
+    sunk = []
+    
+    position = 0
+    for xaxis in range(8):
+        rows = ""
+        for yaxis in range(8):
+            water = " ~ "
+            if position in hit:
+                water = " X "
+            elif position in miss:
+                water = " - "
+            elif position in sunk:
+                water = " @ "
+            rows = rows + water
+            position += 1
+        print(xaxis, rows)
+
+run_game()
+
+
+
+
+
+
+
+
