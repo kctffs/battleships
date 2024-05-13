3ec79610e65676361dedfdcd600a3b4dc5658801
Kieran Cattermole <kctffs@gmail.com>
2024-05-13 02:04:35 +0000

    initial commit defining functions

--- run.py
+++ run.py
@@ -1,3 +1,24 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+class Board:
+    """
+    Class for the games board which will hold lists, create the boards size, number of battleships,
+    and the players name. Class will also print.
+    """
+    def __init__(self, size, num_ships, player_name):
+
+def validate_chosen_guess():
+    """
+    In place to secure guesses are playable from the player
+    """
+
+def chosen_guess():
+    """
+    Creating the input for the coord guessed by the player on the board.
+    """
+
+
+def run_game():
+    """
+    Starts the battleships game and creates the board by correlating with the Board class; choosing
+    size, number of battleships and name. Will reset previous plays.
+    """
+
