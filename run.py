1b00202e5df4a1a9ecc1adb7aaad7b529387c17a
Kieran Cattermole <kctffs@gmail.com>
2024-05-13 14:30:58 +0000

    waves now being replaced with an X when hit in the water

--- run.py
+++ run.py
@@ -1,10 +1,15 @@
-
 print("   0  1  2  3  4  5  6  7 ")
+
+hit = [63]
+position = 0
 for xaxis in range(8):
     rows = ""
     for yaxis in range(8):
         water = " ~ "
+        if position in hit:
+            water = " X "
         rows = rows + water
+        position += 1
     print(xaxis, rows)
 
 
@@ -29,6 +34,9 @@ for xaxis in range(8):
     and the players name. Class will also print.
     """
     #def __init__(self, size, num_ships, player_name):
+        #self.size = size
+        #self.num_ships = num_ships
+        #self.player_name = player_name
 
 #def validate_chosen_guess():
     """
