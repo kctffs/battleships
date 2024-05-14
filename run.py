94cbc4990175f1c3f7614887418639b25ff7a2c3
Kieran Cattermole <kctffs@gmail.com>
2024-05-14 00:05:10 +0000

    changed starting position of the board to 1 from 0 for clearer understanding when creating player guess placement

--- run.py
+++ run.py
@@ -1,10 +1,13 @@
 from random import randint, shuffle
 
-
+"""
 def investigate_ship(length_ships, start_ships, direction):
-    """
-    Investigates the ships length, starting placement and the direction the ship is facing
-    """
+
+    
+    Investigates the ships length, starting placement and the direction 
+    the ship is facing
+    
+
     ship = []
     if direction == "N":
         for num in range(length_ships):
@@ -23,7 +26,6 @@ def investigate_ship(length_ships, start_ships, direction):
             ship.append(start_ships - num)
             print(start_ships - num)
 
-
 ships = [4]
 for length_ships in ships:
     create_ships = randint(0, 63)
@@ -31,15 +33,18 @@ for length_ships in ships:
     shuffle(direction_ships)
     print(length_ships, create_ships, direction_ships[0])
     investigate_ship(length_ships, create_ships, direction_ships[0])
-
 """
+
 def run_game(hit, miss, sunk):
-    
-    Creates the board for the game and prints it and holds lists for the players actions
-    
+
+    """
+    Creates the board for the game and prints it and holds lists for 
+    the players actions
+    """
+
     print("   0  1  2  3  4  5  6  7 ")
-    
-    position = 0
+
+    position = 1
     for xaxis in range(8):
         rows = ""
         for yaxis in range(8):
@@ -55,9 +60,8 @@ def run_game(hit, miss, sunk):
             position += 1
         print(xaxis, rows)
 
-hit = []
+hit = [8, 16, 24, 32, 40, 48, 56, 64]
 miss = []
 sunk = []
 
 run_game(hit, miss, sunk)
-"""
